71ab99679fd32456863c8527645f499279cc8393
lilynnn <pinksoltlatte@gmail.com>
2022-02-08 11:24:47 +0900

    add: ReplyVo필드 추가 author:누리
    ReplyVo필드 추가

--- heyworks/src/main/java/com/kh/hey/common/model/vo/Reply.java
+++ heyworks/src/main/java/com/kh/hey/common/model/vo/Reply.java
@@ -1,5 +1,23 @@
 package com.kh.hey.common.model.vo;
 
+import java.sql.Date;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
 public class Reply {
+	
+	private String replyNo;
+	private String userNo;
+	private String replyContent;
+	private Date createDate;
+	private String refBoardNo;
+	private String status;
 
 }
