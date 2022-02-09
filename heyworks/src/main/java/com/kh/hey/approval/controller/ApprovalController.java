dea7327382320279b07bebcf463dd33fdc800db6
lilynnn <pinksoltlatte@gmail.com>
2022-02-09 11:38:52 +0900

    correct: mapper파일 수정 author:누리
    correct: mapper파일 수정

--- heyworks/src/main/java/com/kh/hey/approval/controller/ApprovalController.java
+++ heyworks/src/main/java/com/kh/hey/approval/controller/ApprovalController.java
@@ -1,12 +1,16 @@
 package com.kh.hey.approval.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.kh.hey.approval.model.vo.Approval;
+import com.kh.hey.approval.model.service.ApprovalService;
 
 @Controller
 public class ApprovalController {
+	
+	@Autowired
+	private ApprovalService aService;
 	
 	@RequestMapping("main.el")
 	public String mainList() {
