a5c2614aaea7878038dcf9e6691fc4fc699138f6
zzse <tpgus0274@gmail.com>
2022-02-10 21:37:22 +0900

    update:mybatis-config.xml수정 author:세현

--- heyworks/src/main/java/com/kh/hey/reserve/model/service/ReservationServiceImpl.java
+++ heyworks/src/main/java/com/kh/hey/reserve/model/service/ReservationServiceImpl.java
@@ -1,13 +1,23 @@
 package com.kh.hey.reserve.model.service;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.kh.hey.reserve.model.dao.ReservationDao;
 import com.kh.hey.reserve.model.vo.Reservation;
 
 
 
 @Service
 public class ReservationServiceImpl implements ReservationService {
+	
+	@Autowired
+	private ReservationDao mDao;
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
 
 	@Override
 	public Reservation myReserveList(Reservation re) {
